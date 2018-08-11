17a61f62ee96106da1b43b93b5f268c385a4245d
Robert Faldo <robertfaldo@gmail.com>
2018-08-11 09:19:13 +0100

    functional components

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    console.log(this.props.value)
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -23,7 +17,6 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
-
 
   handleClick(i) {
     const currentSquares = this.state.squares.slice()
